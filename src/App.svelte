15342cf4c0cdd52f4e7b104b7eeeed55ca5c547f
teatov <hlebpoimenigleb@gmail.com>
2025-11-02 05:52:30 +1000

    add basic page info

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
 </script>
 
-<main>
+<main class="flex h-full items-center justify-center">
+  <div class="max-w-xl p-4">
+    <p class="mb-4">
+      Find the languages that one Wikipedia article has, but the other doesn't.
+    </p>
+    <p class="mt-4">
+      Made by <a href="https://teatov.xyz" class="font-semibold underline"
+        >Teatov</a
+      >. Source code available on
+      <a
+        href="https://github.com/teatov/wiki-lang-comparer"
+        class="font-semibold underline">GitHub</a
+      >.
+    </p>
+  </div>
 </main>
